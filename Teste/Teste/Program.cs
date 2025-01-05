44c32a5287e1b3c9f4ee01bf5b3a77940079fae6
mihneaclgr <mihneaclgr@gmail.com>
2025-01-05 19:41:22 +0200

    FirstCommit

---
+++ Teste/Teste/Program.cs
@@ -0,0 +1,15 @@
+﻿using System.IO;
+
+void AdaugareRuta()
+{
+    string plecare, destinatie;
+    int nrKm;
+    
+    plecare = Console.ReadLine();
+    destinatie = Console.ReadLine();
+    nrKm = int.Parse(Console.ReadLine());
+    
+    File.AppendAllText("Ruterute.txt",plecare+"-"+destinatie+", "+nrKm+'\n');
+}
+
+AdaugareRuta();
